6947848a4bf1bbee5836beb44e79cd29a6fde01b
Arcachofo <santigoro@mailfence.com>
2025-05-03 09:39:57 +0200

    Cleaning.

--- hw/gpio/esp32_gpio.c
+++ hw/gpio/esp32_gpio.c
@@ -27,8 +27,7 @@ static uint64_t esp32_gpio_read( void *opaque, hwaddr addr, unsigned int size )
     uint64_t r = 0;
 
     if( addr < 0x88 ){
-        switch( addr )
-        {
+        switch( addr ){
         case 0x04: r = gpioS->gpio_out;    break; //GPIO_OUT_REG
         case 0x20: r = gpioS->gpio_enable; break; //GPIO_ENABLE_REG
         case 0x38: r = gpioS->strap_mode;  break; //A_GPIO_STRAP:
@@ -46,45 +45,22 @@ static uint64_t esp32_gpio_read( void *opaque, hwaddr addr, unsigned int size )
         case 0x68: r = gpioS->gpio_pcpu_int[0]; break; //GPIO_PCPU_INT_REG
         case 0x74: r = gpioS->gpio_acpu_int[1]; break; //GPIO_ACPU_INT1_REG
         case 0x7c: r = gpioS->gpio_pcpu_int[1]; break; //GPIO_PCPU_INT1_REG
-        default: break;
         }
     }
-    else if( addr >= 0x88 && addr < 0x130 ) { //GPIO_PINXX_REG
-        int n = (addr - 0x88) / 4;
-        r = gpioS->gpio_pin[n];
-    }
-    else if( addr >= 0x130 && addr < 0x530 ) { //GPIO_FUNCY_IN_SEL_CFG_REG
-        int n = (addr - 0x130) / 4;
-        r = gpioS->gpio_in_sel[n];
-    }
-    else if( addr >= 0x530 && addr < 0x5d0 ) { //GPIO_FUNCX_OUT_SEL_CFG_REG
-        int n = (addr - 0x530) / 4;
-        r = gpioS->gpio_out_sel[n];
-    }
-    return r;
-}
+    else if( addr < 0x130 ) r = gpioS->gpio_pin[(addr-0x88)/4];     //GPIO_PINXX_REG
+    else if( addr < 0x530 ) r = gpioS->gpio_in_sel[(addr-0x130)/4]; //GPIO_FUNCY_IN_SEL_CFG_REG
+    else if( addr < 0x5d0 ) r = gpioS->gpio_out_sel[(addr-0x530)/4];//GPIO_FUNCX_OUT_SEL_CFG_REG
 
-static int get_triggering( int int_type, int oldval, int val ) //Interrupt type selection
-{
-    switch( int_type )
-    {
-    case 1: return (val >  oldval); //rising edge trigger
-    case 2: return (val <  oldval); //falling edge trigger
-    case 3: return (val != oldval); //any edge trigger
-    case 4: return (val == 0);      //low level trigger
-    case 5: return (val == 1);      //high level trigger
-    }
-    return 0; //GPIO interrupt disable
+    return r;
 }
 
 static void inputChanged( void *opaque, int n, int val )
 {
     Esp32GpioState *gpioS = ESP32_GPIO( opaque );
 
-    int n1;
-    int i;
-    if( n < 32 ) { n1 = n;    i = 0; }
-    else         { n1 = n-32; i = 1; }
+    int n1 = n;
+    int i = 0;
+    if( n >= 32 ) { n1 -= 32; i = 1; }
 
     uint32_t* pcpuInt = &gpioS->gpio_pcpu_int[i];
     uint32_t* acpuInt = &gpioS->gpio_acpu_int[i];
@@ -96,19 +72,22 @@ static void inputChanged( void *opaque, int n, int val )
 
     uint32_t gpioPin = gpioS->gpio_pin[n];
     int int_type = (gpioPin >> 7) & 7;
-    int irq = get_triggering( int_type, oldval, val );
 
-    // says bit 16 in the ref manual, is that wrong?
-    if( irq && (gpioPin & (1 << 15)) )   // pro cpu int enable
+    bool irq = 0;
+    switch( int_type )
     {
-        qemu_set_irq( gpioS->irq, 1);
-        *pcpuInt |= (1 << n1);
+    case 1: irq = (val >  oldval); break; //rising edge trigger
+    case 2: irq = (val <  oldval); break; //falling edge trigger
+    case 3: irq = (val != oldval); break; //any edge trigger
+    case 4: irq = (val == 0);      break; //low level trigger
+    case 5: irq = (val == 1);      break; //high level trigger
     }
-    if( irq && (gpioPin & (1 << 13)) )   // app cpu int enable
-    {
-        qemu_set_irq( gpioS->irq, 1);
-        *acpuInt |= (1 << n1);
-    }
+    if( !irq ) return;                    // RETURN, no irq
+
+    qemu_set_irq( gpioS->irq, 1);
+
+    if( gpioPin & (1 << 15) ) *pcpuInt |= (1 << n1); // pro cpu int enable // says bit 16 in the ref manual, is that wrong?
+    if( gpioPin & (1 << 13) ) *acpuInt |= (1 << n1); // app cpu int enable
 }
 
 static void clearStatus( Esp32GpioState* gpioS, int n, uint64_t value )
@@ -143,8 +122,7 @@ static void esp32_gpio_write( void *opaque, hwaddr addr, uint64_t value, unsigne
     uint32_t oldEnable = gpioS->gpio_enable;
 
     if( addr < 0x88 ){
-        switch( addr )
-        {
+        switch( addr ){
         case 0x04: gpioS->gpio_out        =  value; break; // GPIO_OUT_REG
         case 0x08: gpioS->gpio_out       |=  value; break; // GPIO_OUT_W1TS_REG
         case 0x0C: gpioS->gpio_out       &= ~value; break; // GPIO_OUT_W1TC_REG
@@ -154,34 +132,32 @@ static void esp32_gpio_write( void *opaque, hwaddr addr, uint64_t value, unsigne
         case 0x38: gpioS->strap_mode      =  value; break; // A_GPIO_STRAP
         case 0x44: gpioS->gpio_status[0]  =  value; break; // GPIO_STATUS_REG
         case 0x48: gpioS->gpio_status[0] |=  value; break; // GPIO_STATUS_W1TS_REG
-        case 0x4c: clearStatus( gpioS, 0, value );  break; // GPIO_STATUS_W1TC_REG
+        case 0x4C: clearStatus( gpioS, 0, value );  break; // GPIO_STATUS_W1TC_REG
         case 0x50: gpioS->gpio_status[1]  =  value; break; // GPIO_STATUS1_REG
         case 0x54: gpioS->gpio_status[1] |=  value; break; // GPIO_STATUS1_W1TS_REG
         case 0x58: clearStatus( gpioS, 1, value );  break; // GPIO_STATUS1_W1TC_REG
         }
     }
-    else if( addr >= 0x88 && addr < 0x130) {            //GPIO_PINXX_REG
-        int n = (addr - 0x88) / 4;
-        gpioS->gpio_pin[n] = value;
+    else if( addr < 0x130) {                               //GPIO_PINXX_REG
+        gpioS->gpio_pin[(addr - 0x88)/4] = value;
     }
-    else if( addr >= 0x130 && addr < 0x530) {           //GPIO_FUNCY_IN_SEL_CFG_REG
-        int n = (addr - 0x130) / 4;
+    else if( addr < 0x530) {                               //GPIO_FUNCY_IN_SEL_CFG_REG
+        int n = (addr-0x130)/4;
         gpioS->gpio_in_sel[n] = value;
-        qemu_set_irq( gpioS->conf_irq, (0x1000 | n) );  //report in sel cfg change
+        qemu_set_irq( gpioS->conf_irq, (0x1000 | n) ); //report in sel cfg change
     }
-    else if( addr >= 0x530 && addr < 0x5d0) {           //GPIO_FUNCX_OUT_SEL_CFG_REG
-        int n = (addr - 0x530) / 4;
+    else if( addr < 0x5d0) {                               //GPIO_FUNCX_OUT_SEL_CFG_REG
+        int n = (addr-0x530)/4;
         gpioS->gpio_out_sel[n] = value;
-        qemu_set_irq( gpioS->conf_irq, (0x2000 | n) );  //report out sel cfg change
+        qemu_set_irq( gpioS->conf_irq, (0x2000 | n) ); //report out sel cfg change
     }
-
     //printf("out 0x%04X in 0x%04X enable 0x%04X \n",gpioS->gpio_out, gpioS->gpio_in, gpioS->gpio_enable);
 
     uint32_t enableDiff = gpioS->gpio_enable ^ oldEnable;
     uint32_t outDiff    = gpioS->gpio_out    ^ oldOut;
     uint32_t changed    = outDiff | enableDiff;
 
-    if( !changed ) return;      // RETURN, no pin changed
+    if( !changed ) return;                       // RETURN, no pin changed
 
     for( int i=0; i<32; i++ )
     {
@@ -204,7 +180,7 @@ static void esp32_gpio_write( void *opaque, hwaddr addr, uint64_t value, unsigne
 }
 
 static const MemoryRegionOps uart_ops = {
-    .read =  esp32_gpio_read,
+    .read  = esp32_gpio_read,
     .write = esp32_gpio_write,
     .endianness = DEVICE_LITTLE_ENDIAN,
 };
@@ -252,17 +228,17 @@ static void esp32_gpio_class_init( ObjectClass *klass, void *data )
     DeviceClass *dc = DEVICE_CLASS(klass);
 
     dc->legacy_reset = esp32_gpio_reset;
-    dc->realize = esp32_gpio_realize;
+    dc->realize      = esp32_gpio_realize;
     device_class_set_props( dc, esp32_gpio_properties );
 }
 
 static const TypeInfo esp32_gpio_info = {
-    .name = TYPE_ESP32_GPIO,
-    .parent = TYPE_SYS_BUS_DEVICE,
+    .name          = TYPE_ESP32_GPIO,
+    .parent        = TYPE_SYS_BUS_DEVICE,
     .instance_size = sizeof(Esp32GpioState),
     .instance_init = esp32_gpio_init,
-    .class_init = esp32_gpio_class_init,
-    .class_size = sizeof(Esp32GpioClass),
+    .class_init    = esp32_gpio_class_init,
+    .class_size    = sizeof(Esp32GpioClass),
 };
 
 static void esp32_gpio_register_types(void)
